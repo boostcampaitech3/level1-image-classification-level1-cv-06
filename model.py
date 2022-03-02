4325d3d189deea730d0796b4f0385232758c6a95
Juyoung Kim <ingressor531@gmail.com>
2022-03-02 22:23:42 +0900

    EfficientNet_b3 실험 업로드

--- model.py
+++ model.py
@@ -11,15 +11,15 @@ class ExperimentalClassifier(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-        self.fc = nn.Linear(2048, 18, bias=True)
+        self.fc = nn.Linear(1536, 18, bias=True)
         torch.nn.init.kaiming_normal_(self.fc.weight)
         torch.nn.init.zeros_(self.fc.bias)
 
-        self.effnetb5 = models.efficientnet_b5(pretrained=True)
-        self.effnetb5.classifier = nn.Sequential(
-            nn.Dropout(p=0.4, inplace=True),
+        self.effnetb3 = models.efficientnet_b3(pretrained=True)
+        self.effnetb3.classifier = nn.Sequential(
+            nn.Dropout(p=0.3, inplace=True),
             self.fc
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.effnetb5(x)
+        return self.effnetb3(x)
