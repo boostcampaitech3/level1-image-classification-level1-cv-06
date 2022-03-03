6b667d1b95a4b87ae569c50e37a0b8f41bce27c7
Yewon-dev <kyw20160785@gmail.com>
2022-03-04 00:25:06 +0900

    baseline commit

--- model.py
+++ model.py
@@ -1,39 +1,76 @@
-#
-# MultiHeadClassifier
-#
-
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.models as models
+#import timm
 
 class MultiHeadClassifier(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,num_classes) -> None:
         super().__init__()
 
-        self.resnet18 = models.resnet18(pretrained=True)
-        self.resnet18.fc = nn.Identity()
-        for param in self.resnet18.parameters():
-            param.requires_grad = False
+        self.model = models.vgg19_bn(pretrained=True)
+        self.model.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
 
-        self.fc_mask = nn.Linear(512, 3, bias=True)
-        self.fc_gender = nn.Linear(512, 2, bias=True)
-        self.fc_age = nn.Linear(512, 3, bias=True)
-
-        torch.nn.init.kaiming_normal_(self.fc_mask.weight)
-        torch.nn.init.kaiming_normal_(self.fc_gender.weight)
-        torch.nn.init.kaiming_normal_(self.fc_age.weight)
-        torch.nn.init.zeros_(self.fc_mask.bias)
-        torch.nn.init.zeros_(self.fc_gender.bias)
-        torch.nn.init.zeros_(self.fc_age.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.resnet18(x)
+        x = self.model(x)
 
-        mask = self.fc_mask(x)
-        gender = self.fc_gender(x)
-        age = self.fc_age(x)
+        return x
 
-        mask_gender = torch.einsum("ij,ik->ijk", (mask, gender)).reshape(mask.shape[0], -1)
-        mask_gender_age = torch.einsum("ij,ik->ijk", (mask_gender, age)).reshape(mask_gender.shape[0], -1)
 
-        return mask_gender_age
+class BaseModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.25)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout2(x)
+
+        x = self.avgpool(x)
+        x = x.view(-1, 128)
+        return self.fc(x)
+
+
+# Custom Model Template
+class MyModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        """
+        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
+        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
+        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
+        """
+
+    def forward(self, x):
+        """
+        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
+        2. 결과로 나온 output 을 return 해주세요
+        """
+        return x
